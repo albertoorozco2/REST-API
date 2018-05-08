53e115068d3aa5a5250590d2093b3c83854da616
A1 <albertoorozco2@hotmail.com>
2018-05-08 20:46:10 +0100

    comments added

--- src/frontend/client.ts
+++ src/frontend/client.ts
@@ -3,9 +3,9 @@
 
 let newUser = { email: Math.random().toString(36).substring(7)+"@gmail.com", password: "secret" };
 let defaultUser = { email: "albertoorozco7@gmail.com", password: "secret" };
-
+//web client 
 console.log("\nWEB CLIENTE");
-
+  // create a new user by /api/v1/users POST create a new User
 async function createUser(){
 	console.log("\nhttp://127.0.0.1:3000/api/v1/users POST create a new User");
 	console.log("   Create a user :"+ JSON.stringify(newUser.email));
@@ -34,7 +34,7 @@ async function createUser(){
 }
 
 
-
+  // auth users by /api/v1/auth/login POST authentificate a User
 async function authUser(){
 	console.log("\nhttp://127.0.0.1:3000/api/v1/auth/login POST authentificate a User");
 	console.log("   authentificating of default user :"+ JSON.stringify(defaultUser.email));
@@ -63,6 +63,7 @@ async function authUser(){
 }
 
 (async ()=>{
+  // execute the create user function with a random email
 await createUser()
 .then(function(result) {
    console.log("JSON response:") 
@@ -70,7 +71,7 @@ await createUser()
 })
 
 
-
+  // execute the auth user function with the default user
 await authUser()
 .then(function(result) {
    console.log("   JSON response:") 
